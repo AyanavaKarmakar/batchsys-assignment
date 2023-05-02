aa16da2dd5abfae903e3abae34b7b4d28a1119db
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-05-03 00:43:16 +0530

    feat: add discounts

--- components/ProductCard1.tsx
+++ components/ProductCard1.tsx
@@ -22,7 +22,26 @@ export const ProductCard1 = ({ product }: { product: Product }) => {
 
         <div className="flex flex-col w-full">
           <div className="font-bold text-xl">{product.title}</div>
-          <div className="font-bold text-lg">${product.price}</div>
+
+          {product.discountPercentage > 0 ? (
+            <div className="font-bold text-base">
+              <span className="line-through text-gray-500">
+                ${product.price.toFixed(2)}
+              </span>
+              <span className="ml-2">
+                $
+                {(
+                  product.price -
+                  (product.price * product.discountPercentage) / 100
+                ).toFixed(2)}
+              </span>
+            </div>
+          ) : (
+            <div className="font-bold text-base">
+              ${product.price.toFixed(2)}
+            </div>
+          )}
+
           <div className="mt-1 flex flex-row">
             <Ratings product={product} />
           </div>
--- components/ProductCard2.tsx
+++ components/ProductCard2.tsx
@@ -13,10 +13,29 @@ export const ProductCard2 = ({ product }: { product: Product }) => {
         height={400}
       />
 
-      <div className="flex flex-col w-full">
+      <div className="flex flex-col w-full leading-none">
         <div className="flex flex-row justify-between">
-          <div className="font-bold text-xl">{product.title.toUpperCase()}</div>
-          <div className="font-bold text-xl">${product.price}</div>
+          <div className="font-bold text-base">
+            {product.title.toUpperCase()}
+          </div>
+          {product.discountPercentage > 0 ? (
+            <div className="font-bold text-base">
+              <span className="line-through text-gray-500">
+                ${product.price.toFixed(2)}
+              </span>
+              <span className="ml-2">
+                $
+                {(
+                  product.price -
+                  (product.price * product.discountPercentage) / 100
+                ).toFixed(2)}
+              </span>
+            </div>
+          ) : (
+            <div className="font-bold text-base">
+              ${product.price.toFixed(2)}
+            </div>
+          )}
         </div>
 
         <div className="mt-1 flex flex-row">
