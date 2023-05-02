73d70aa091973d5ad7951db39afa5c04cf520f8a
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-05-02 21:01:25 +0530

    fix product card styles

--- components/ProductCard1.tsx
+++ components/ProductCard1.tsx
@@ -2,7 +2,7 @@ import { Product } from "../utils/ProductSchema";
 
 export const ProductCard1 = ({ product }: { product: Product }) => {
   return (
-    <div className="flex flex-col gap-3 items-center justify-center min-h-screen">
+    <div className="flex flex-col gap-3 items-center justify-center">
       <h1 className="text-6xl font-bold text-center">Product Card 1</h1>
     </div>
   );
